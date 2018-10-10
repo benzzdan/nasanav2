2e715dfed561e4b99d33c3ac0b785ffaac0f9a17
DANIEL BENSON <bensondaniel664@gmail.com>
2018-10-10 13:25:29 -0500

    changed migrations table

--- database/migrations/2018_10_04_220641_create_cards_table.php
+++ database/migrations/2018_10_04_220641_create_cards_table.php
@@ -15,7 +15,7 @@ class CreateCardsTable extends Migration
     {
         Schema::create('cards', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('list_id');
+            $table->integer('lists_id');
             $table->string('name');
             $table->string('description');
             $table->timestamps();
